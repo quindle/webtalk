6779ab268b20f7ee3b767cb903f4560ce455a9d2
Alessandro Rinaldi <alerinaldi81@hotmail.it>
2023-12-27 00:21:51 +0100

    initialize project

---
+++ libconsumer/somefile.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var webtalklib_1 = require("webtalklib");
+(0, webtalklib_1.sayHello)();
---
+++ libconsumer/somefile.ts
@@ -0,0 +1,2 @@
+import { sayHello } from 'webtalklib'
+sayHello();
